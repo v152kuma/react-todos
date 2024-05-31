0b2466b25960557a85a9745f34d2680100c6d860
unknown <vivek.kumar450028@gmail.com>
2024-06-01 01:19:11 +0530

    adding tables

--- src/App.js
+++ src/App.js
@@ -2,11 +2,36 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  /*this is a commnet*/
   return (
+
     <div className="App">
-     <p>
-     Learn React  with Vivek
-     </p>
+    <div>
+      <table>
+        <tr>
+          <th>Index</th>
+          <th>Work</th>
+          <th>Assigned to</th>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>LeetCode</td>
+          <td>Person 1</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>ByteByteGo</td>
+          <td>Person 2</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td>Cimpress</td>
+          <td>Person 3</td>
+        </tr>
+      </table>
+   </div>
+
       
     </div>
   );
